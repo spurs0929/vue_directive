918c320fa1e790561d8e49c124f610189f2ebd70
spurs0929 <briankang0929@gmail.com>
2022-07-31 18:03:37 +0800

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <tab 
+    :tabData="tabData"
+    :initial-index="initialIndex"
+  ></tab>  
+</template>
+
+<script>
+
+import Tab from './components/Tab.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Tab
+  },
+  data(){
+    return {
+      tabData: [
+        {
+          id: 1,
+          title: '選項1',
+          content: '內容1'
+        },
+        {
+          id: 2,
+          title: '選項2',
+          content: '內容2'
+        },
+        {
+          id: 3,
+          title: '選項3',
+          content: '內容3'
+        },
+        {
+          id: 4,
+          title: '選項4',
+          content: '內容4'
+        },
+      ],
+      initialIndex: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
